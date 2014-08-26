78c28846be11955e37463f9dcc7d915b0a5b6efc
Swaagie <info@martijnswaagman.nl>
2014-08-26 15:26:45 +0200

    [minor] add example

---
+++ example/pages/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var Page = require('bigpipe').Page
+  , pagelet = require('../../');
+
+Page.extend({
+  path: '/',              // HTTP route we should respond to.
+  view: './index.hbs',    // The base template we need to render.
+  pagelets: {             // The pagelets that should be rendered.
+    list: pagelet.extend({
+      data: [{
+        id: 'knockout',
+        name: 'KnockoutJS',
+        link: 'http://knockoutjs.com/',
+        downloads: 10,
+        followers: 20,
+      }, {
+        id: 'angular',
+        name: 'AngularJS',
+        link: 'https://angularjs.org/',
+        downloads: 110,
+        followers: 8
+      }, {
+        id: 'meteor',
+        name: 'Meteor',
+        link: 'https://www.meteor.com/',
+        downloads: 2099,
+        followers: 4
+      }]
+    })
+  }
+}).on(module);
