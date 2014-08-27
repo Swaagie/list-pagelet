597ce1bc056b9a9bc4fc6ae5e5b51846b5493855
Swaagie <info@martijnswaagman.nl>
2014-08-27 17:17:47 +0200

    [fix] calculate height of container on the fly due to lack of flow

--- client.js
+++ client.js
@@ -2,7 +2,9 @@ pipe.once('list:initialize', function init(pagelet) {
   'use strict';
 
   var base = $(pagelet.placeholders)
-    , order = base.find('.order');
+    , order = base.find('.order')
+    , collection = base.find('.collection')
+    , items = collection.find('.item');
 
   /**
    * Update the absolute position of each item.
@@ -12,7 +14,7 @@ pipe.once('list:initialize', function init(pagelet) {
    */
   function sort(data) {
     data.forEach(function each(item, i) {
-      base.find('#' + item.id).css('transform', 'translate3d(0,'+ item.y +',0)');
+      collection.find('#' + item.id).css('transform', 'translate3d(0,'+ item.y +',0)');
     });
   }
 
@@ -22,4 +24,14 @@ pipe.once('list:initialize', function init(pagelet) {
   order.on('change', function change() {
     pagelet.sort($(this).find(':selected').val(), sort);
   });
+
+  //
+  // Update the height of the container if the list of items does not fit the
+  // visible height of the container. This is required since the items are
+  // absolutely positioned and will not have flow.
+  //
+  var height = items.length * items.outerHeight() * 1.2
+    , current = collection.height();
+
+  if (current < height) collection.height(height);
 });
